a84ae8e5c3ef304fc22e4ffbb38b2c75261178a0
David Castellar Martinez <castellardav@gmail.com>
2025-04-02 08:45:59 -0500

    feat: add middleware for validating user fields

--- src/infrastructure/adapters/input/rest/routes/userRoutes.ts
+++ src/infrastructure/adapters/input/rest/routes/userRoutes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { UserController } from "../controllers/userController";
 import { MongoUserRepository } from "../../../output/mongo/mongoUserRepository";
 import { CreateUserInputPort } from "../../../../../application/ports/input/user/createUserInputPort";
+import { validateUserFields } from "../middlewares/validateUserFields";
 
 export const userRouter = express.Router();
 
@@ -9,4 +10,4 @@ const userRepository = new MongoUserRepository();
 const createUserUseCase = new CreateUserInputPort(userRepository);
 const userController = new UserController(createUserUseCase);
 
-userRouter.post("/", userController.create);
+userRouter.post("/", validateUserFields, userController.create);
